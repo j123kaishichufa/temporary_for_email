3866068005af50efc7066a698f5c1399a3be53d5
j123kaishichufa <j123kaishichufa@163.com>
2016-03-19 12:14:57 +0800

    add apache cxf.xml file

--- clientpoint/rpctest/src/com/mkyong/client/HelloWorldClient.java
+++ clientpoint/rpctest/src/com/mkyong/client/HelloWorldClient.java
@@ -11,7 +11,7 @@ public class HelloWorldClient
 	public static void main(String[] args) throws Exception 
 	{
 	   
-		URL url = new URL("http://192.168.0.107:9999/ws/hello?wsdl");
+		URL url = new URL("localhost:9999/ws/hello?wsdl");
 	
         //1st argument service URI, refer to wsdl document above
 		//2nd argument is service name, refer to wsdl document above
@@ -21,7 +21,7 @@ public class HelloWorldClient
 
         HelloWorld hello = service.getPort(HelloWorld.class);
 
-        System.out.println(hello.getHelloWorldAsString("HAHAHAHAHAHAHA"));
+        System.out.println(hello.getHelloWorldAsString("TEST TEST TEST XXXXXXXXXXXXXXXXX"));
 
     }
 
--- serverpoint/rpctest/src/com/mkyong/endpoint/HelloWorldPublisher.java
+++ serverpoint/rpctest/src/com/mkyong/endpoint/HelloWorldPublisher.java
@@ -9,7 +9,7 @@ public class HelloWorldPublisher
 	
 	public static void main(String[] args) 
 	{
-	   Endpoint.publish("http://192.168.0.107:9999/ws/hello", new HelloWorldImpl());
+	   Endpoint.publish("http://localhost:9999/ws/hello", new HelloWorldImpl());
     }
 
 }
