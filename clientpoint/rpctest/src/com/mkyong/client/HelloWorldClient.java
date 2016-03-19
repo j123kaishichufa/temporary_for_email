1eb3e890396eaa8bc482d62e579e7d4d01bf535e
j123kaishichufa <j123kaishichufa@163.com>
2016-03-19 15:45:02 +0800

    add http protocol

--- clientpoint/rpctest/src/com/mkyong/client/HelloWorldClient.java
+++ clientpoint/rpctest/src/com/mkyong/client/HelloWorldClient.java
@@ -11,7 +11,7 @@ public class HelloWorldClient
 	public static void main(String[] args) throws Exception 
 	{
 	   
-		URL url = new URL("localhost:9999/ws/hello?wsdl");
+		URL url = new URL("http://127.0.0.1:9999/ws/hello?wsdl");
 	
         //1st argument service URI, refer to wsdl document above
 		//2nd argument is service name, refer to wsdl document above
